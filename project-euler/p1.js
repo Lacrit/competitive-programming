909ec02289bd2662df59cddc4a4e0831ce7dba98
lacrit <lacrit.pravis@gmail.com>
2019-10-28 13:50:33 +0100

    wooorks

--- project-euler/p1.js
+++ project-euler/p1.js
@@ -23,5 +23,5 @@ let total1 = 0;
 // 3 + 6 + .. + 999 = 3 * (1 + 2 + 3 + .. + 333) = 3 * ((333*(333+1))/2)
 // 5 + 10 + ... + 995 = 5 * (1 + 2 + 3 + .. + 199) = 5 * ((199*(199+1))/2)
 // 15 + 30 + ... + 990 = 15 * (1 + 2 + 3 + .. + 66) = 15 * ((66*(66+1))/2)
-const multiplesOfXYSum = (x, n) => x*((n*(n+1))/2)
-total = multiplesOfXYSum(3, 333) + multiplesOfXYSum(5, 199) - multiplesOfXYSum(15, 66);
+const multiplesOfXYSum = (x, n) => x*((Math.floor(n/x)*(Math.floor(n/x)+1))/2)
+total = multiplesOfXYSum(3, 1000) + multiplesOfXYSum(5, 1000) - multiplesOfXYSum(15, 1000);
