be74dd59de0222cce8f5a1e87fedfdaddde1f919
lacrit <lacrit.pravis@gmail.com>
2022-12-05 00:09:44 +0100

    advent: task 3 - part 1 done

--- advent-of-code-22/3/rucksack-reorganisation.ts
+++ advent-of-code-22/3/rucksack-reorganisation.ts
@@ -10,27 +10,27 @@ const formatInputData = (fileData: string): string[][]  =>  fileData.split("\n")
 
 // ==================================================================
 
-//  upper case letter priorities are 27 >= x <= 52, lower case letter priorities are 1 >= x <= 27,
+//  upper case letter priorities are 28 > x <= 52, lower case letter priorities are 1 >= x < 27,
 const getCharPriority = (char: string | null): number => {
     if (!char) return 0;
     const charASCII = char.charCodeAt(0);
-    return char === char.toUpperCase() ?  27 + charASCII -'A'.charCodeAt(0) : 1 +  charASCII -'a'.charCodeAt(0);
+    return char === char.toUpperCase() ?  27 + charASCII-'A'.charCodeAt(0) : 1 +  charASCII-'a'.charCodeAt(0);
 
 }  
 
-const findRucksackError = (compartment1: Set<string>, compartment2: Set<string>): string => 
+const findCompartmentsError = (compartment1: Set<string>, compartment2: Set<string>): string => 
     [compartment1, compartment2].reduce((prevValue: Set<string>, currValue: Set<string>) => 
         new Set(Array.from(prevValue).filter((char: string) => currValue.has(char)))
     ).values().next().value;
 
 
-const findCompartmentError = (compartments: string[][]) => 
-    compartments.reduce((prevValue: number, currValue: string[]) => 
-        prevValue + getCharPriority(findRucksackError(new Set(currValue[0]), new Set(currValue[1]))), 
+const findRucksackError = (compartments: string[][]) => 
+    compartments.reduce((prevValue: number, [compartment1, compartment2]: string[]) => 
+        prevValue + getCharPriority(findCompartmentsError(new Set(compartment1), new Set(compartment2))), 
     0);
 
 
 const inputDataPart1 = formatInputData(file);
-const part1 = findCompartmentError(inputDataPart1);
+const part1 = findRucksackError(inputDataPart1);
 
 console.log(part1)
